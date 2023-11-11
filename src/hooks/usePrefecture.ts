eda75463df1996c0fd72fb560cdf1c89f0e37f47
Yusei Mizutani <yusei4520030401@gmail.com>
2023-11-12 00:28:31 +0900

    add : 都道府県取得のフックスの実装

---
+++ src/hooks/usePrefecture.ts
@@ -0,0 +1,18 @@
+import { useCallback, useState } from 'react';
+
+import { fetchPrefectures } from '@/api/prefectures';
+import { PrefectureNextResponse } from '@/types/prefecture';
+
+export const usePrefecture = () => {
+  const [prefectures, setPrefecture] = useState<PrefectureNextResponse>();
+
+  const getPrefecture = useCallback(async () => {
+    const prefectureRes = await fetchPrefectures();
+    if (!prefectureRes) {
+      return;
+    }
+    setPrefecture(prefectureRes);
+  }, []);
+
+  return { getPrefecture, prefectures };
+};
