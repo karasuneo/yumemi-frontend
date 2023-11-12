a2589a6c7e0bfa411505a44f30695e1b7ff63916
Yusei Mizutani <yusei4520030401@gmail.com>
2023-11-12 13:06:42 +0900

    add : グラフタイトルの対応定数を定義

--- src/const/index.ts
+++ src/const/index.ts
@@ -1,3 +1,11 @@
+export const POPULATION_TYPE: { [key: string]: string } = {
+  none: '',
+  total: '総人口',
+  under: '年少人口',
+  working: '生産年齢人口',
+  elderly: '老年人口',
+};
+
 export const POPULATION_TYPE_NUM: { [key: string]: number } = {
   total: 0,
   under: 1,
