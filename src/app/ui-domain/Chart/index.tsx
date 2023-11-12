15dbe009567f9835ad5d763885ef5f932aee3bb4
Yusei Mizutani <yusei4520030401@gmail.com>
2023-11-12 12:56:29 +0900

    add : 折れ線グラフのUIコンポーネントを作成

---
+++ src/app/ui-domain/Chart/index.tsx
@@ -0,0 +1,53 @@
+'use client';
+
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  ChartData,
+  ChartDataset,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+);
+
+type Props = {
+  title: string;
+  labels: number[] | string[];
+  datasets: ChartDataset<'line', number[]>[];
+};
+
+export function Chart({ title, labels, datasets }: Props) {
+  const data: ChartData<'line', number[], number | string> = {
+    labels: labels || [],
+    datasets: datasets || [],
+  };
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top' as const,
+      },
+      title: {
+        display: true,
+        text: title,
+      },
+    },
+  };
+
+  return <Line options={options} data={data} />;
+}
