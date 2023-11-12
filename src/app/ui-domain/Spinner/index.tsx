a5b97f52f863999d833d20b4005ca1b276c19597
Yusei Mizutani <yusei4520030401@gmail.com>
2023-11-12 10:40:30 +0900

    update : スピナーを中央に寄せるよう修正

--- src/app/ui-domain/Spinner/index.tsx
+++ src/app/ui-domain/Spinner/index.tsx
@@ -1,5 +1,11 @@
 import styles from './style.module.scss';
 
 export function Spinner() {
-  return <div className={styles.spinner}></div>;
+  return (
+    <div className={styles.overlay}>
+      <div className={styles.overlayInner}>
+        <div className={styles.spinner}></div>
+      </div>
+    </div>
+  );
 }
