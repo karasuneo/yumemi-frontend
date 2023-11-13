21abb4dcb11a2eb974137e232d7157b8c5ddf9d7
Yusei Mizutani <yusei4520030401@gmail.com>
2023-11-13 15:30:22 +0900

    add : 人口構成カードコンポーネントの追加

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,6 @@
 import type { Preview } from '@storybook/react';
 
+import '../src/app/styles/svg.scss';
 import '../src/app/styles/color.scss';
 import '../src/app/styles/variables.scss';
 import '../src/app/styles/typography.scss';
