22b00dddb115b5876c6a256372fc7310a2eb9d5d
Yusei Mizutani <yusei4520030401@gmail.com>
2023-11-13 15:29:08 +0900

    add : ヘッダーの追加

---
+++ src/app/ui-domain/Header/index.tsx
@@ -0,0 +1,9 @@
+import styles from './style.module.scss';
+
+export function Header() {
+  return (
+    <header className={styles.container}>
+      <h1>ゆめみフロントエンドコーディングテスト</h1>
+    </header>
+  );
+}
